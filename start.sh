8b95ad359ede459fb51114396624f5f51374f30a
utzuro <utzuro@pm.me>
2025-11-08 10:35:40 +0900

    feat: build with Dockerimage

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# for GUI to work
+xhost +local:docker
+
+# get the current directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+docker build -t zathura . &&
+	docker run -it \
+		--env DISPLAY=$DISPLAY \
+		--volume /tmp/.X11-unix:/tmp/.X11-unix \
+		--volume $DIR:/app \
+		zathura:latest
