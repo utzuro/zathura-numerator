f31714914521437334aa3ff6138a34ea1d724a23
utzuro <utzuro@pm.me>
2026-08-25 15:54:16 +0900

    update Nix shell for GTK4

--- shell.nix
+++ shell.nix
@@ -1,31 +1,41 @@
-with import <nixpkgs> { };
-stdenv.mkDerivation {
-  name = "build";
-  buildInputs = [
-    meson
-    pkgconf
-    pkg-config
+let
+  pkgs = import <nixpkgs> { };
+  girara = pkgs.girara.overrideAttrs (_: {
+    version = "2026.07.18";
+    src = pkgs.fetchFromGitHub {
+      owner = "pwmt";
+      repo = "girara";
+      tag = "2026.07.18";
+      hash = "sha256-Q4IbB8Wecob9NH6UPqyIifyd3D+IpMCfe725U3htR+s=";
+    };
+    mesonFlags = [ "-Ddocs=disabled" ];
+    doCheck = false;
+  });
+in
+pkgs.mkShell {
+  inputsFrom = [ pkgs.zathura ];
+
+  packages = with pkgs; [
+    file
     gettext
-    autoconf
-    automake
-    libtool
-    sphinx
-    ffmpeg
-    texinfo
-    SDL2
-    gtk3
-    glib
-    glibc
-    json-glib
-    sqlite
-    libffi
-    zlib
-    libmediainfo
-    libffi
-    libseccomp
     girara
-    shared-mime-info
-    libnotify
-    doxygen
+    glib
+    gtk4
+    json-glib
+    libdatrie
+    libseccomp
+    libselinux
+    libsepol
+    libsysprof-capture
+    libthai
+    libxdmcp
+    meson
+    ninja
+    pkg-config
+    pkgconf
+    pcre2
+    sqlite
+    util-linux
+    xxhash
   ];
 }
